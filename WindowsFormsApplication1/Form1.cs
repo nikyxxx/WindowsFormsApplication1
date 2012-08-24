a394712e19b9597834330be16d73c770eb519d05
nikyxxx <works_nbb@163.com>
2012-08-24 21:17:18 +0800

    ddddd

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -19,6 +19,7 @@ namespace WindowsFormsApplication1
         private void Form1_Load(object sender, EventArgs e)
         {
 //changed test!
+            //tt3
         }
     }
 }
--- WindowsFormsApplication1/Program.cs
+++ WindowsFormsApplication1/Program.cs
@@ -13,6 +13,7 @@ namespace WindowsFormsApplication1
         [STAThread]
         static void Main()
         {
+            //tt2
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
